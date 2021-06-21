58bede7455f2f749790b6608048bfb18003222fd
kyoya <kyoyaprog@gmail.com>
2021-06-21 23:07:09 +0900

    SearchRepositoryModelを実装

--- iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
@@ -1,7 +1,15 @@
 import Foundation
 
-class SearchRepositoryModel:SearchRepositoryModelInput{
+class SearchRepositoryModel: SearchRepositoryModelInput {
     func searchRepositories(keyword: String, completion: @escaping (Result<[Repository], Error>) -> Void) {
-        
+        APIClient().send(SearchRepositoryTarget(keyword: keyword)) { responce in
+            switch responce {
+            case .success(let repositories):
+                completion(.success(repositories.items))
+
+            case .failure(let error):
+                completion(.failure(error as NSError))
+            }
+        }
     }
 }
