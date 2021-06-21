0576505cf706d23a0fa0bc0d3df129b842aff5cc
kyoya <kyoyaprog@gmail.com>
2021-06-21 19:47:29 +0900

    swiftlintで指摘されたエラーやワーニングを修正

--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -9,51 +9,47 @@
 import UIKit
 
 class ViewController2: UIViewController {
-    
-    @IBOutlet weak var ImgView: UIImageView!
-    
-    @IBOutlet weak var TtlLbl: UILabel!
-    
-    @IBOutlet weak var LangLbl: UILabel!
-    
-    @IBOutlet weak var StrsLbl: UILabel!
-    @IBOutlet weak var WchsLbl: UILabel!
-    @IBOutlet weak var FrksLbl: UILabel!
-    @IBOutlet weak var IsssLbl: UILabel!
-    
+    @IBOutlet weak var imgView: UIImageView!
+
+    @IBOutlet weak var ttlLbl: UILabel!
+
+    @IBOutlet weak var langLbl: UILabel!
+
+    @IBOutlet weak var strsLbl: UILabel!
+    @IBOutlet weak var wchsLbl: UILabel!
+    @IBOutlet weak var frksLbl: UILabel!
+    @IBOutlet weak var isssLbl: UILabel!
+
     var vc1: ViewController?
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let idx = vc1?.idx,let repo = vc1?.repo[idx] else { return }
-        
-        LangLbl.text = "Written in \(repo.language)"
-        StrsLbl.text = "\(repo.stargazersCount) stars"
-        WchsLbl.text = "\(repo.watchersCount) watchers"
-        FrksLbl.text = "\(repo.forksCount) forks"
-        IsssLbl.text = "\(repo.openIssuesCount) open issues"
+        guard let idx = vc1?.idx, let repo = vc1?.repo[idx] else { return }
+
+        langLbl.text = "Written in \(repo.language)"
+        strsLbl.text = "\(repo.stargazersCount) stars"
+        wchsLbl.text = "\(repo.watchersCount) watchers"
+        frksLbl.text = "\(repo.forksCount) forks"
+        isssLbl.text = "\(repo.openIssuesCount) open issues"
         getImage(repository: repo)
-        
     }
-    
-    func getImage(repository:Repository){
-        
-        TtlLbl.text = repository.fullName
-        
+
+    func getImage(repository: Repository) {
+        ttlLbl.text = repository.fullName
+
         let owner = repository.owner
         let imgURL = owner.avatarUrl
-        if let url = URL(string: imgURL){
-            URLSession.shared.dataTask(with: url) { (data, res, err) in
-                if let error = err{
+        if let url = URL(string: imgURL) {
+            URLSession.shared.dataTask(with: url) { data, _, err in
+                if let error = err {
                     print(error)
                 }
-                if let data = data,let image = UIImage(data: data) {
+                if let data = data, let image = UIImage(data: data) {
                     DispatchQueue.main.async { [weak self] in
-                        self?.ImgView.image = image
+                        self?.imgView.image = image
                     }
                 }
             }.resume()
         }
     }
-    
 }
