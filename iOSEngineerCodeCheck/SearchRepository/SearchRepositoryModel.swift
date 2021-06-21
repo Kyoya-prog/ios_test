281e02242f4d906d7505d33d237aea75636d0d6d
kyoya <kyoyaprog@gmail.com>
2021-06-21 21:36:21 +0900

    ファイルの整理

---
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class SearchRepositoryModel:SearchRepositoryModelInput{
+    func searchRepositories(keyword: String, completion: @escaping (Result<[Repository], Error>) -> Void) {
+        
+    }
+}
