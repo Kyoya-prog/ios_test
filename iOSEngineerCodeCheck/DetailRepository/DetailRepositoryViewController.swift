b8ca1b8b65c9c8298fcb2ce1dd4f84677a49dbcf
kyoya <kyoyaprog@gmail.com>
2021-06-23 23:31:32 +0900

    ViewControllerの背景色を設定

--- iOSEngineerCodeCheck/DetailRepository/DetailRepositoryViewController.swift
+++ iOSEngineerCodeCheck/DetailRepository/DetailRepositoryViewController.swift
@@ -14,6 +14,8 @@ class DetailRepositoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+
         setUp()
     }
 
--- iOSEngineerCodeCheck/SearchRepository/SearchRepositoryViewController.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryViewController.swift
@@ -17,6 +17,7 @@ class SearchRepositoryViewController: UIViewController, SearchRepositoryPresente
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
 
         setUp()
     }
