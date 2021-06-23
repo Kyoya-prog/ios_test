23eb86dfc796a275e5b0d9c8c82833ab05a88e2a
kyoya <kyoyaprog@gmail.com>
2021-06-23 22:01:16 +0900

    SearchRepositoryViewControllerにおいて、Viewをコードで表現するようにする

--- iOSEngineerCodeCheck/SearchRepository/SearchRepositoryViewController.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryViewController.swift
@@ -8,9 +8,7 @@
 
 import UIKit
 
-class SearchRepositoryViewController: UITableViewController, SearchRepositoryPresenterOutput {
-    @IBOutlet weak var searchBar: UISearchBar!
-
+class SearchRepositoryViewController: UIViewController, SearchRepositoryPresenterOutput {
     /// 表示するリポジトリ一覧
     var repositories: [Repository] = []
 
@@ -28,23 +26,51 @@ class SearchRepositoryViewController: UITableViewController, SearchRepositoryPre
 
     func updateRepositories(repositories: [Repository]) {
         self.repositories = repositories
-        tableView.reloadData()
+        repositoriesView.reloadData()
     }
 
     func transitionToRepositoryDetail(repository: Repository) {
         presentDetailRepositoryView(repository: repository)
     }
-    
+
     // MARK: Private
     private func presentDetailRepositoryView(repository: Repository) {
         let detailViewController = ModuleAssembler.assembleDetailRepositoryModule(repository: repository)
         navigationController?.pushViewController(detailViewController, animated: true)
     }
-    
-    private func setUp(){
-        searchBar.text = "ripozitoriwokensaku"
+
+    private func setUp() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
         searchBar.delegate = self
+        searchBar.placeholder = "リポジトリを検索"
+        view.addSubview(searchBar)
+
+        repositoriesView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(repositoriesView)
+
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+
+            repositoriesView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            repositoriesView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            repositoriesView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            repositoriesView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
+
+    private let searchBar = UISearchBar()
+
+    private lazy var repositoriesView: UITableView = {
+        let view = UITableView()
+        view.delegate = self
+        view.dataSource = self
+        view.backgroundColor = .clear
+        view.separatorStyle = .none
+        view.register(SearchRepositoryResultCell.self, forCellReuseIdentifier: SearchRepositoryResultCell.reuseIdentifer)
+        return view
+    }()
 }
 
 // MARK: UISearchBarDelegate
@@ -65,21 +91,23 @@ extension SearchRepositoryViewController: UISearchBarDelegate {
 }
 
 // MARK: UITableViewController
-extension SearchRepositoryViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension SearchRepositoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         repositories.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // swiftlint:disable:next force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchRepositoryResultCell.reuseIdentifer) as! SearchRepositoryResultCell
         let repository = repositories[indexPath.row]
-        cell.textLabel?.text = repository.fullName
-        cell.detailTextLabel?.text = repository.language
-        cell.tag = indexPath.row
+        cell.userName = repository.name
+        cell.repositoryName = repository.owner.login
+        cell.language = repository.language
+        cell.profileImageURL = URL(string: repository.owner.avatarUrl)
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         presenterInput.didSelectRepository(repository: repositories[indexPath.row])
     }
 }
