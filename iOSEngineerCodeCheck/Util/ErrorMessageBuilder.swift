361f1ced9d54a9805a9ec56aa9ed15dc358e5edf
kyoya <kyoyaprog@gmail.com>
2021-06-25 01:14:36 +0900

    コメントを追加

--- iOSEngineerCodeCheck/Util/ErrorMessageBuilder.swift
+++ iOSEngineerCodeCheck/Util/ErrorMessageBuilder.swift
@@ -6,9 +6,10 @@ import UIKit
 class ErrorMessageBuilder {
     static func buildErrorMessage(error: Error, message: String) -> String {
         var errorMessage = message
+        // NSError型を得るためMoyaErrorの中のNSUnderlyingErrorを抽出し、NSErrorにキャスト
         if let moyaError = error as? MoyaError, let error = ((moyaError.errorUserInfo["NSUnderlyingError"]as? Alamofire.AFError)?.underlyingError as NSError?) {
             switch error.code {
-            case -1009: // NSURLErrorNotConnectedToInternet
+            case -1009: // NSURLErrorNotConnectedToInternet　（ネットワーク接続エラーの場合は、その趣旨をエラーメッセージの後につける）
                 errorMessage = message + "(インターネット接続がありません)"
             default:
                 errorMessage = message
